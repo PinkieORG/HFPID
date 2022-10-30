d22f71fa1ce3704a14f4581de6967480385f304e
x473839 <pinkie?MUNI2>
2022-10-30 22:36:51 +0100

    added test to model

--- dataset.py
+++ dataset.py
@@ -1,5 +1,6 @@
 from glob import glob
 from PIL import Image
+from torch import nn
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
 
@@ -22,4 +23,26 @@ class Imagenette2(Dataset):
     def __getitem__(self, id):
         image_path = self.images[id]
         image = Image.open(image_path).convert('RGB')
+        image = self.transform(image)
+        if self.mode == 'test':
+            ref_image = nn.functional.interpolate(image, scale_factor=0.5)
+            return image, ref_image
         return self.transform(image)
+
+
+class OneImage(Dataset):
+    def __init__(self, image_path, input_size=512):
+        self.image_path = image_path
+        self.input_size = input_size
+        self.transform = transforms.Compose([transforms.Resize((input_size, input_size)),
+                                             transforms.ToTensor(),
+                                             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, id):
+        image = Image.open(self.image_path).convert('RGB')
+        image = self.transform(image)
+        ref_image = nn.functional.interpolate(image, scale_factor=0.5)
+        return image, ref_image
--- train.py
+++ train.py
@@ -7,8 +7,11 @@ from torch import optim
 import torch.nn as nn
 import pytorch_lightning as pl
 from torchmetrics import StructuralSimilarityIndexMeasure as SSIM
+from torchvision.transforms import transforms
+from torchvision.utils import make_grid, save_image
+
 import models
-from dataset import Imagenette2
+from dataset import Imagenette2, OneImage
 
 
 def get_args():
@@ -35,6 +38,15 @@ def get_args():
     parser.add_argument('--beta1', default=0.9, type=int)
     parser.add_argument('--beta2', default=0.999, type=int)
     parser.add_argument('--eps', default=0.00000001, type=int)
+
+    # For test mode.
+    parser.add_argument('--weights', default='', help="If path to file with weights is passed then program will enter test mode."
+                                                      "Input image(s) will be pre-resized to 2 times of the input_size"
+                                                      "of the loaded model and fed to the encoder part of the model."
+                                                      "Will use validation dataset on default. If you want to use"
+                                                      "other image then specify test_file argument (only one at the time).")
+    parser.add_argument('--test_image', default='', help="Image to downscale when in test mode.")
+    parser.add_argument('--test_output_dir', default='test', help="Directory where outputs of testing will be saved.")
     return parser.parse_args()
 
 
@@ -58,6 +70,13 @@ class HFPID(pl.LightningModule):
 
     def val_dataloader(self):
         dataset = Imagenette2('val', input_size=self.hparams.input_size)
+        return torch.utils.data.DataLoader(dataset, batch_size=self.hparams.batch_size, shuffle=False, num_workers=10)
+
+    def test_dataloader(self):
+        if self.hparams.test_file:
+            dataset = OneImage(self.hparams.test_image, input_size=2 * self.hparams.input_size)
+            return torch.utils.data.DataLoader(dataset, batch_size=1)
+        dataset = Imagenette2('test', input_size=self.hparams.input_size)
         return torch.utils.data.DataLoader(dataset, batch_size=self.hparams.batch_size, shuffle=False, num_workers=10)
 
     def configure_optimizers(self):
@@ -90,6 +109,17 @@ class HFPID(pl.LightningModule):
         loss = loss / len(outputs)
         self.log('loss', loss)
 
+    def test_step(self, x, xid):
+        inv_transform = transforms.Compose([transforms.Normalize([0, 0, 0], [1 / 0.229, 1 / 0.224, 1 / 0.225]),
+                                            transforms.Normalize([-0.485, -0.456, -0.406], [1, 1, 1])])
+        y = self.decoder(x[0])
+        y = inv_transform(y)
+        ref = inv_transform(x[1])
+        images = torch.zeros((2 * x.size[0], *x.size[1:]))
+        images[::2, :, :, :] = ref
+        images[1::2, :, :, :] = y
+        save_image(images, fp=Path(self.hparams.test_output_dir, 'test_output.jpg'), nrow=6)
+
 
 if __name__ == '__main__':
     args = get_args()
@@ -111,4 +141,10 @@ if __name__ == '__main__':
                          check_val_every_n_epoch=args.check_val_every_n_epoch,
                          accelerator='gpu',
                          devices=2)
-    trainer.fit(pl_model)
+    if args.weights:
+        pl_model = HFPID.load_from_checkpoint(args.weights,
+                                              test_image = args.test_image,
+                                              test_output_dir = args.test_output_dir)
+        trainer.test(pl_model)
+    else:
+        trainer.fit(pl_model)
